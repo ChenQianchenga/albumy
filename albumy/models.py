5002e63651e364e3c818108659d17540ca5fb6e0
ChenQianchenga <980938541@qq.com>
2023-11-02 22:42:10 +0800

    安装开发所需扩展

--- albumy/models.py
+++ albumy/models.py
@@ -2,7 +2,10 @@
 # --------------------------------------------------------------------------
 # ProjectName：albumy
 # Name:models.py
-# Description:
+# Description: 数据库模型
 # Author:ChenQiancheng
 # Date:2023/11/1  23:31
 # --------------------------------------------------------------------------
+from datetime import datetime
+from flask_login import UserMixin
+from albumy.extensions import db
